50ed9d64c32c0d79afc6fcf1affdae0426871673
RosarioEspadera <s.rvespadera@usm.edu.ph>
2025-07-17 19:22:45 +0800

    Update upload.js

--- src/js/upload.js
+++ src/js/upload.js
@@ -1,7 +1,11 @@
 // src/js/upload.js
 import { createClient } from 'https://esm.sh/@supabase/supabase-js';
 
-const supabase = createClient('YOUR_SUPABASE_URL', 'YOUR_ANON_KEY');
+const supabase = createClient(
+  'https://roqikwfaenwqipdydhwv.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJvcWlrd2ZhZW53cWlwZHlkaHd2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI2MTYxMzksImV4cCI6MjA2ODE5MjEzOX0.CpUCA3X4bNIjOCtxrdOZ2kciXEHEogukBie9IOlHpno'
+);
+
 const form = document.getElementById('uploadForm');
 
 form.addEventListener('submit', async (e) => {
