64eb5eecfffde7cde286e9ff716bf0e2852dcc73
RosarioEspadera <s.rvespadera@usm.edu.ph>
2025-07-18 00:51:44 +0800

    Update upload.js

--- src/js/upload.js
+++ src/js/upload.js
@@ -6,13 +6,24 @@ const supabase = createClient(
   'https://roqikwfaenwqipdydhwv.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJvcWlrd2ZhZW53cWlwZHlkaHd2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI2MTYxMzksImV4cCI6MjA2ODE5MjEzOX0.CpUCA3X4bNIjOCtxrdOZ2kciXEHEogukBie9IOlHpno', // anon key
   {
+   (async () => {
+  const { data, error } = await supabase.auth.getSession();
+
+  if (error || !data.session) {
+    alert("You're not signed in.");
+    return;
+  }
+
+  const token = data.session.access_token;
+
+  const supabaseWithToken = createClient('https://roqikwfaenwqipdydhwv.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJvcWlrd2ZhZW53cWlwZHlkaHd2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI2MTYxMzksImV4cCI6MjA2ODE5MjEzOX0.CpUCA3X4bNIjOCtxrdOZ2kciXEHEogukBie9IOlHpno', {
     global: {
       headers: {
-        Authorization: `Bearer ${token}`
-      }
-    }
-  }
-);
+        Authorization: `Bearer ${token}`,
+      },
+    },
+  });
 
 const form = document.getElementById('uploadForm');
 const submitBtn = form.querySelector('button[type="submit"]');
