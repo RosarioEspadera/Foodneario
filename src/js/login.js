f0290334db654f921e74c078a570cd9aae29f9b5
RosarioEspadera <s.rvespadera@usm.edu.ph>
2025-07-17 19:10:12 +0800

    Update login.js

--- src/js/login.js
+++ src/js/login.js
@@ -1,1 +1,7 @@
-.
+// src/js/login.js
+export function handleCredentialResponse(response) {
+  const jwt = response.credential;
+  console.log("Google JWT:", jwt);
+  localStorage.setItem("token", jwt); // store for later use
+}
+window.handleCredentialResponse = handleCredentialResponse;
